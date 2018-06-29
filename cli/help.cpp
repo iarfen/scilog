778adf6e3184389af6bd2a0f267230306b6a00ca
Ismael Correa <ismael.correa.castro@gmail.com>
2018-06-29 19:36:59 -0400

    help message ordered alphabetically

--- cli/help.cpp
+++ cli/help.cpp
@@ -13,6 +13,8 @@ namespace scilog_cli
 		cout << "commands:" << endl;
 		cout << "    --help,-h                              display this help message" << endl;
 		cout << "    --version,-v                           display the version of scilog" << endl << endl;
+		cout << "    create-month-file <month-number>       creates the file of the given month" << endl;
+		cout << "    create-year-files                      creates all the files of the year" << endl << endl;
 		cout << "    list <month-number> [year]             list all the entries of the selected month" << endl;
 		cout << "    list --kind=learn|project              list only entries of the selected kind" << endl;
 		cout << "    list --kind=learn" << endl
@@ -30,17 +32,15 @@ namespace scilog_cli
 		cout << "    open [year] --topics                   opens the file of topics of the selected year" << endl << endl;
 		cout << "    open-dir                               opens the directory of the actual year" << endl;
 		cout << "    open-dir [year]                        opens the directory of the selected year" << endl << endl;
+		cout << "    show-categories                        list all the categories and subcategories" << endl << endl;
 		cout << "    summary                                summarizes the work of the year" << endl;
 		cout << "    summary <month-number> [year]          summarizes the work of the month or year selected" << endl << endl;
 		cout << "    summary-topics                         summarizes the topics worked and learned" << endl;
 		cout << "    summary-topics <month-number> [year]   summarizes the topics of the month or year selected" << endl << endl;
 		cout << "    summary-sciences                       summarizes the sciences worked on" << endl;
 		cout << "    summary-sciences <month-number> [year] summarizes the sciences of the month or year selected" << endl << endl;
-		cout << "    create-month-file <month-number>       creates the file of the given month" << endl << endl;
-		cout << "    create-year-files                      creates all the files of the year" << endl << endl;
 		cout << "    validate                               validates all the files of the folder (both months and topics files)" << endl;
-		cout << "    validate <month-number>                validates the file of the given month" << endl << endl;
-		cout << "    validate --topics                      validates the topics.scilog_topics file" << endl << endl;
-		cout << "    show-categories                        list all the categories and subcategories" << endl;
+		cout << "    validate <month-number>                validates the file of the given month" << endl;
+		cout << "    validate --topics                      validates the topics.scilog_topics file" << endl;
 	}
 }
