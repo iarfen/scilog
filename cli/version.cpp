0608c5a8a8e49153f8022aa810a606619343cf13
Ismael Correa <ismael.correa.castro@gmail.com>
2018-07-23 21:37:31 -0400

    scilog now displays the author name, email address, among other important information

--- cli/version.cpp
+++ cli/version.cpp
@@ -11,5 +11,8 @@ namespace scilog_cli
 	void command_version()
 	{
 		cout << PACKAGE_NAME << " " << PACKAGE_VERSION << endl;
+		cout << "Copyright (C) " << PACKAGE_YEAR << " " << PACKAGE_AUTHOR << " " << PACKAGE_BUGREPORT << endl;
+		cout << PACKAGE_WEBSITE << endl << endl;
+		cout << "This program is free software, you can redistribute it under the terms of the GPLv3. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE." << endl;
 	}
 }
